f5ecd42ce777cb90164584b614a89de63cf7c48b
rotem plaut <33826133+rotempl@users.noreply.github.com>
2018-03-19 21:25:41 +0200

    Auto-generated commit

--- src/consts/activity.ts
+++
@@ -1,6 +0,0 @@
-export const elements = [
-    { name: 'Nina Jones', time: 'Just now', acativity: `added a new project`, acticityName: 'Free UI Kit', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmiYBfagv2fN00YfTmLD_LFgV8uNWmC1vzczcFu6sTttMHuVgI' }, 
-    { name: 'James Smith', time: '40 minutes ago', acativity: `commented project`, acticityName: 'Free PSD Template', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS73teVVSM3qArByHWHG86lxhgWNJSFiArVG5YXnzMHWwRugZI5' }, 
-    { name: 'Alex Clooney', time: '1 hour ago', acativity: `completed task`, acticityName: 'Symu Website', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-l66VoqS9KftIULAiL6BxRXyyHDN8Bfc0syRjZKsouEWdHpHQ' }, 
-    { name: 'Alexandra Spears', time: '3 hours ago', acativity: `added a new project`, acticityName: 'Free PSD(...)', image: 'https://pmcdeadline2.files.wordpress.com/2017/03/rachelgriffiths.png?w=446&h=299&crop=1' }
-];
